fb04999add300ade62abe7e3ab544e6c15004303
Michaël Wizman <michaelwizman@hotmail.com>
2014-10-27 20:03:45 +0100

    Small correction on ZohoClient's attributes declaration

--- src/Zoho/CRM/ZohoClient.php
+++ src/Zoho/CRM/ZohoClient.php
@@ -61,7 +61,7 @@ class ZohoClient
 	 * 
 	 * @param string $authtoken Token for connection
 	 * @param HttpClientInterface $client HttpClient for connection [optional]
-	 * @param FactotoryInterface $factory [optional]
+	 * @param FactoryInterface $factory [optional]
 	 */
 	public function __construct($authtoken, HttpClientInterface $client = null, FactoryInterface $factory = null)
 	{
