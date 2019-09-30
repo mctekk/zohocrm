bf2b031c509955e689e416567bff3cadd7777b12
Rafael White <rwhite@mctekk.com>
2019-09-30 14:16:18 -0400

    Add examples for inserting and updating lead record

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ use Zoho\CRM\Entities\Lead;
 use Zoho\CRM\ZohoClient;
 
 $ZohoClient = new ZohoClient(
-    '1000.f5cada9783878bbafb5cd10706deded7.f2f85eec81d9c3cd98066a201578417e',
+    '1000.d78edac4fb4a56e64338b884a91a9a58.0ba8f8f69a139ddbb6cf386bd0035ff9',
     '1000.NCUTHYFLRLZYRX57IMA205E1UBDUFH',
     '53d352fd313851fc558de0595039a03950b5d1cba5',
     'https://www.zoho.com'
@@ -21,32 +21,24 @@ $refresh = $ZohoClient->generateAccessTokenByRefreshToken();
 
 // $response = $ZohoClient->getRecords();
 
-$response = $ZohoClient->getRecordById('2896936000050692283');
+// $response = $ZohoClient->getRecordById('2896936000050692283');
+
+// $response = $ZohoClient->searchRecords('(Email:testlololololo13@gmail.com)');
+
+$lead = new Lead();
+
+// Receiving request
+$request = [
+    'first_name' => 'Test32',
+    'last_name' => 'Test2'
+    // 'email' => 'aisrxybja22@sharklasers.com',
+    // 'phone' => '4043123124225'
+];
+
+$data = $lead->deserializeXml($lead->serializeXml($request));
+
+// $response = $ZohoClient->insertRecords($data, ['wfTrigger' => 'true']);
+$response = $ZohoClient->updateRecords('2896936000050971263', $data, ['wfTrigger' => 'true']);
 
 print_r($response);
 die();
-
-// $lead = new Lead();
-// // $lead->deserializeXml($xmlstr);
-
-// // Receiving request
-// $request = [
-//     'first_name' => 'Test',
-//     'last_name' => 'Test',
-//     'email' => 'aisrxybja2@sharklasers.com',
-//     'phone' => '404-855-2695',
-//     'affiliate_record%5FID' => '95641000016912544',
-// ];
-
-// $xmlstr2 = $lead->serializeXml($request); // Mapping the request for create xmlstr
-// $lead->deserializeXml($xmlstr2);
-
-// $ZohoClient = new ZohoClient('YOU_TOKEN'); // Make the connection to zoho api
-// $ZohoClient->setModule('Leads'); // Selecting the module
-// $validXML = $ZohoClient->mapEntity($lead); // Create valid XML (zoho format)
-
-// // Insert the new record
-// $response = $ZohoClient->insertRecords($validXML, ['wfTrigger' => 'true']);
-
-// print_r($response);
-// print "\n";
