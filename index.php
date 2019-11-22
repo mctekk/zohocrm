2bd9cdd358a636028edf5e1f9f32bd9872294d28
Rafael White <rwhite@mctekk.com>
2019-11-22 17:20:16 -0400

    Add owner field

--- index.php
+++ index.php
@@ -10,6 +10,14 @@ use Zoho\CRM\ZohoClient;
 
 $ZohoClient = new ZohoClient();
 
+$ZohoClient->setAuthRefreshToken('1000.5c4728219eb163a276d47132143a9257.de9ff3dfb628ca36c22240a5fbb529d8');
+$ZohoClient->setZohoClientId('1000.NCUTHYFLRLZYRX57IMA205E1UBDUFH');
+$ZohoClient->setZohoClientSecret('53d352fd313851fc558de0595039a03950b5d1cba5');
+$refresh = $ZohoClient->generateAccessTokenByRefreshToken();
+
+// print_r($refresh);
+// die();
+
 $ZohoClient->setModule('Leads');
 
 // $ZohoClient->generateAccessTokenByGrantToken();
@@ -20,7 +28,19 @@ $ZohoClient->setModule('Leads');
 
 // $response = $ZohoClient->getRecordById('2896936000050692283');
 
-// $response = $ZohoClient->searchRecords('(Email:testlololololo13@gmail.com)');
+// $response = $ZohoClient->searchRecords('(Email:c00000exa27@sharklasers.com)')->getRecords();
+
+// for ($i = 0; $i < 5; $i++) {
+//     for ($j = 0; $j < 7; $j++) {
+//         // $response = $ZohoClient->searchRecords('(Email:c00000exa5@sharklasers.com)')->getRecords();
+//         $refresh = $ZohoClient->generateAccessTokenByRefreshToken();
+//         var_dump($refresh);
+//         sleep(1);
+//     }
+//     sleep(5);
+// }
+
+// die();
 
 $lead = new Lead();
 
@@ -33,20 +53,29 @@ $lead = new Lead();
 // ];
 
 $request = [
-    'Lead_Owner' => 'mark@financefactory.com',
+    // 'Owner' => 'mark@financefactory.com',
+    'Owner' => '2896936000004024001',
     'First_Name' => 'disposabile111',
     'Last_Name' => 'leadtest111',
     'Lead_Source' => 'Christian Guthermann',
-    'Phone' => '004234253543',
-    'Email' => 'c0000010014@sharklasers.com',
-    'Member' => '23',
+    'Phone' => '22223425363447',
+    'Email' => 'c00000exa29@sharklasers.com',
+    'Member' => '33',
     'Sponsor' => '0000',
     'Code' => '110100001570806179',
-    'URL' => "https => \/\/api.thefinancefactory.com\/Christian-Guthermann",
+    'URL_1' => 'https://lp.thefinancefactory.com/lp/r/23',
     'Affiliate_RecordE5FID' => 95641000006185231,
-    'Sales_Rep' => 'Mark Ledford'];
+    'Sales_Rep' => 'Mark Ledford',
+    'Available Collateral' => json_decode(json_encode(['real estate,stock portfolio']), true)
+];
+
+// print_r($request);
+// die();
 
 $data = $lead->deserializeXml($lead->serializeXml($request));
+
+// print_r($data);
+// die();
 
 $response = $ZohoClient->insertRecords($data, ['wfTrigger' => 'true']);
 // $response = $ZohoClient->updateRecords('2896936000050971263', $data, ['wfTrigger' => 'true']);
@@ -54,5 +83,6 @@ $response = $ZohoClient->insertRecords($data, ['wfTrigger' => 'true']);
 // $ZohoClient->setModule('Users');
 // $response = $ZohoClient->getUsers('AllUsers', 1)->getRecords();
 
+// print_r($refresh);
 print_r($response);
 die();
